4e7beccba1bebd8e0fdcc61407dc8370d49b4822
kraigmansfield <Kraig.mansfield@gmail.com>
2023-01-12 13:18:34 -0900

    updates

--- assets/script.js
+++ assets/script.js
@@ -1,56 +1,56 @@
 //jQuery selectors
 
 // Games Data API Connection
-var game1 = document.querySelector("#game1");
+var game1 = document.querySelector('#game1')
 
 const options = {
-  method: "GET",
+  method: 'GET',
   headers: {
-    "X-RapidAPI-Key": "fc98b95498mshba10dc92df84595p1eeda7jsne575863c62de",
-    "X-RapidAPI-Host": "steam-special-offers.p.rapidapi.com",
+    'X-RapidAPI-Key': 'fc98b95498mshba10dc92df84595p1eeda7jsne575863c62de',
+    'X-RapidAPI-Host': 'steam-special-offers.p.rapidapi.com',
   },
-};
+}
 var fetchsteam = function (data) {
-	console.log('hello')
+  console.log('hello')
   fetch(
-    "https://steam-special-offers.p.rapidapi.com/games_data/?app_id=271590",
-    options
+    'https://steam-special-offers.p.rapidapi.com/games_data/?app_id=271590',
+    options,
   )
     .then((data) => {
-      return data.json();
+      return data.json()
     })
     .then((completedata) => {
-		console.log(completedata);
-    //   for (var i = 0; i < completedata.length; i++) {
-    //     var gameName = json.stringify(completedata.results[i]);
-    //     console.log(gameName);
-	// 	printGame(completedata[i]);
-    //   }
-	  printGame(completedata)
-    });
+      console.log(completedata)
+      //   for (var i = 0; i < completedata.length; i++) {
+      //     var gameName = json.stringify(completedata.results[i]);
+      //     console.log(gameName);
+      // 	printGame(completedata[i]);
+      //   }
+      printGame(completedata)
+    })
 
   // console.log(completedata[0].title);
 
   // console.log(completedata[0].link);
-};
+}
 // console.log(completedata[0].children[3]);
 
-fetchsteam();
+fetchsteam()
 function printGame(completedata) {
-  console.log(completedata);
-  var gameCard = document.createElement("div");
-  var resultBody = document.createElement("div");
-  gameCard.append(resultBody);
-  var titleEl = document.createElement("h3");
-  titleEl.textContent = completedata.title;
-  var bodyText = document.createElement("p");
-  bodyText.textContent = completedata.price;
-  var linkButtonEl = document.createElement("a");
-  linkButtonEl.textContent = "Go to game";
-  linkButtonEl.setAttribute("href", completedata.url);
+  console.log(completedata)
+  var gameCard = document.createElement('div')
+  var resultBody = document.createElement('div')
+  gameCard.append(resultBody)
+  var titleEl = document.createElement('h3')
+  titleEl.textContent = completedata.title
+  var bodyText = document.createElement('p')
+  bodyText.textContent = completedata.price
+  var linkButtonEl = document.createElement('a')
+  linkButtonEl.textContent = 'Go to game'
+  linkButtonEl.setAttribute('href', completedata.url)
 
-  resultBody.append(titleEl, bodyText, linkButtonEl);
-  game1.append(gameCard);
+  resultBody.append(titleEl, bodyText, linkButtonEl)
+  game1.append(gameCard)
 }
 
 // Recent Gaming News API Connection
