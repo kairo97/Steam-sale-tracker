cdd0c8f273207eac2841a3840c1866c08655b0bc
kraigmansfield <Kraig.mansfield@gmail.com>
2023-01-12 10:40:21 -0900

    updates

--- assets/script.js
+++ assets/script.js
@@ -15,32 +15,26 @@ fetch('https://steam-special-offers.p.rapidapi.com/games_data/?app_id=271590', o
 		return data.json();
 	}).then((completedata)=>{
         console.log(completedata)
-	
-            console.log(completedata[0].title);
-        
-            console.log(completedata[0].link);
-        
+		
         }
 
 ,)
 
-
-
 // Recent Gaming News API Connection
-const optionZ = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'fc98b95498mshba10dc92df84595p1eeda7jsne575863c62de',
-		'X-RapidAPI-Host': 'videogames-news2.p.rapidapi.com'
-	}
-};
+// const optionZ = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'fc98b95498mshba10dc92df84595p1eeda7jsne575863c62de',
+// 		'X-RapidAPI-Host': 'videogames-news2.p.rapidapi.com'
+// 	}
+// };
 
-fetch('https://videogames-news2.p.rapidapi.com/videogames_news/recent', optionZ)
-	.then((data) => {
-		return data.json();
-	}).then((completedata)=>{
-		console.log(completedata)
-})
+// fetch('https://videogames-news2.p.rapidapi.com/videogames_news/recent', optionZ)
+// 	.then((data) => {
+// 		return data.json();
+// 	}).then((completedata)=>{
+// 		console.log(completedata)
+// })
 
     
 
