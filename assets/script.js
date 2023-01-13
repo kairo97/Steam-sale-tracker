e64cf10bdc6c9f6b9c8e77d977c46d565263537d
kraigmansfield <Kraig.mansfield@gmail.com>
2023-01-13 12:22:38 -0900

    git issues

--- assets/script.js
+++ assets/script.js
@@ -4,63 +4,24 @@
 var game1 = document.querySelector('#game1');
 
 const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': 'fc98b95498mshba10dc92df84595p1eeda7jsne575863c62de',
-    'X-RapidAPI-Host': 'steam-special-offers.p.rapidapi.com',
-  },
-}
-// function getRandomNumber(max){
-  // return Math.floor(Math.random()*max);
-// }
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'fc98b95498mshba10dc92df84595p1eeda7jsne575863c62de',
+		'X-RapidAPI-Host': 'steam-special-offers.p.rapidapi.com'
+	}
+};
 
-var steamOffers = "https://steam-special-offers.p.rapidapi.com/games_data/?app_id=271590"
-// +(getRandomNumber(1000)+1);
-var fetchsteam = function (data) {
-  
-  fetch(
-    steamOffers,
-    options,
-  )
-    .then((data) => {
-      return data.json()
-    })
-    .then((completedata) => {
-		console.log(completedata);
-    //   for (var i = 0; i < completedata.length; i++) {
-    //     var gameName = json.stringify(completedata.results[i]);
-    //     console.log(gameName);
-	// 	printGame(completedata[i]);
-    //   }
-    getTitle(completedata);
-	  printGame(completedata);
-    });
+fetch('https://steam-special-offers.p.rapidapi.com/games_data/?app_id=1593500', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
 
-  // console.log(completedata[0].title);
-
-  // console.log(completedata[0].link);
-}
 // console.log(completedata[0].children[3]);
+fetchsteam();
+/*function getTitle(completedata){
+  do{ fetchsteam()} while (completedata.title === null);}*/
 
-//  function getTitle(completedata){
-  // do{ fetchsteam()} while (completedata.title === null);}
 
-function printGame(completedata) {
-  console.log(completedata)
-  var gameCard = document.createElement('div')
-  var resultBody = document.createElement('div')
-  gameCard.append(resultBody)
-  var titleEl = document.createElement('h3')
-  titleEl.textContent = completedata.title
-  var bodyText = document.createElement('p')
-  bodyText.textContent = completedata.price
-  var linkButtonEl = document.createElement('a')
-  linkButtonEl.textContent = 'Go to game'
-  linkButtonEl.setAttribute('href', completedata.url)
-
-  resultBody.append(titleEl, bodyText, linkButtonEl)
-  game1.append(gameCard)
-}
 
 // Recent Gaming News API Connection
 const optionZ = {
@@ -74,11 +35,27 @@ const optionZ = {
 fetch('https://videogames-news2.p.rapidapi.com/videogames_news/recent', optionZ)
 	.then((data) => {
 		return data.json();
-	}).then((completedata)=>{
-		console.log(completedata)
-    
-})
+	}).then((data)=>{
+		console.log(data)
+    console.log(data[0].title)
 
+})
+function printNews(data) {
+  console.log(data)
+  var gameNews = document.createElement('div')
+  var resultBody = document.createElement('div')
+  gameNews.append(resultBody)
+  var titleEl = document.createElement('h3')
+  titleEl.textContent = data[0].title
+  var bodyText = document.createElement('p')
+  bodyText.textContent = data[0].link
+  var linkButtonEl = document.createElement('a')
+  linkButtonEl.textContent = 'Go to game'
+  linkButtonEl.setAttribute('href', data[0].link)
+
+  resultBody.append(titleEl, bodyText, linkButtonEl)
+  gaming-news.append(gameNews)
+}
 // // Games List API Connection
 // const optionY = {
 // 	method: 'GET',
